1d396cee97673c87781b3dfae4a736b89b28ff9b
tre2man <kimtree3940@gmail.com>
2023-07-02 14:51:39 +0900

    guard 리팩터링

--- src/guard/gql-guard.ts
+++ src/guard/gql-guard.ts
@@ -15,42 +15,42 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     // context는 object 객체라서 call by value로 동작한다.
+    const ctx = GqlExecutionContext.create(context).getContext();
+    const token = ctx.req.headers?.authorization;
 
     // 토큰 유효성 검증하기
-    const ctx = GqlExecutionContext.create(context);
-    this.validateToken(ctx);
+    this.validateToken(token);
+
     // context에 유저 정보 입력하기
-    await this.applyUserToContext(ctx);
+    await this.applyUserToContext({ context: ctx, token });
+
     // role 검증하기
-    return this.validateRole(context);
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    return this.validateRole({ context: ctx, roles });
   }
 
-  private validateToken(ctx: GqlExecutionContext) {
-    const token = ctx.getContext().req.headers?.authorization;
+  private validateToken(token: any) {
     if (!token) {
-      throw new GraphQLError('토큰이 잘못되었습니다');
+      throw new GraphQLError('토큰이 존재하지 않습니다.');
     }
     const rawToken = token.split(' ')[1];
     try {
       this.jwtService.verify(rawToken);
     } catch (e) {
-      console.log(e);
       throw new GraphQLError('토큰 검증에 실패했습니다.');
     }
     return rawToken;
   }
 
-  private async applyUserToContext(ctx: GqlExecutionContext) {
-    const rawToken =
-      ctx.getContext().req.headers?.authorization?.split(' ')[1] || '';
+  private async applyUserToContext({ context, token }) {
+    const rawToken = token.split(' ')[1];
     const userId = this.jwtService.decode(rawToken)['id'];
     const user = await this.userService.getUserById(userId);
-    ctx.getContext().user = user;
+    context.user = user;
   }
 
-  private validateRole(context: ExecutionContext): boolean {
-    const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    const userRole = GqlExecutionContext.create(context).getContext().user.role;
+  private validateRole({ context, roles }): boolean {
+    const userRole = context.user.role;
 
     // role이 없으면 모든 권한의 유저들 통과
     if (!roles) {
