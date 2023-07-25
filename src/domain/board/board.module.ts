36e1bba1a8ad1f24bd036704a69f4fa8723a88d3
tre2man <kimtree3940@gmail.com>
2023-07-25 16:36:19 +0900

    필요없는 종속성 제거

--- src/domain/board/board.module.ts
+++ src/domain/board/board.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardResolver } from './board.resolver';
 import { BoardService } from './board.service';
-import { UserService } from 'src/domain/user/user.service';
 import { DatabaseModule } from 'src/db';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [BoardResolver, BoardService, UserService],
+  providers: [BoardResolver, BoardService],
 })
 export class BoardModule {}
