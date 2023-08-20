26203e2c01d37a0897e5809c861aee53334d4ea4
tre2man <kimtree3940@gmail.com>
2023-08-20 22:32:54 +0900

    migration 설정 변경

--- src/db/migration/generate.ts
+++ src/db/migration/generate.ts
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 
 exec(
-  `ts-node -r tsconfig-paths/register --project ./tsconfig.json ./node_modules/.bin/typeorm migration:generate -d ./src/db/migration/migration.ts ./src/db/migration/history/${process.argv[2]}`,
+  `npx typeorm-ts-node-esm migration:generate -d src/db/migration/datasource.ts src/db/migration/history/init${process.argv[2]}`,
   (error, stdout, stderr) => {
     if (error) {
       console.error(error);
--- src/db/migration/run.ts
+++ src/db/migration/run.ts
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 
 exec(
-  'ts-node -r tsconfig-paths/register --project ./tsconfig.json ./node_modules/.bin/typeorm migration:run -d ./src/db/migration/migration.ts',
+  `npx typeorm-ts-node-esm migration:run -d src/db/migration/datasource.ts`,
   (error, stdout, stderr) => {
     if (error) {
       console.error(error);
